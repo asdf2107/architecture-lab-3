f01bcd4f440a16e56f3750a48785e7be0945bc03
Illia Aprol <asdf21072107@gmail.com>
2021-12-05 13:11:26 +0300

    Create tables and insert demo data

---
+++ db/data.sql
@@ -0,0 +1,26 @@
+-- Insert demo data --
+insert into Interests (id, Name) values
+(0, 'it'),
+(1, 'sport'),
+(2, 'food'),
+(3, 'ukraine-politics'),
+(4, 'world-politics');
+
+insert into Forums (id, Name, InterestId) values
+(0, N'Сучасні IT технології', 0),
+(1, N'Здорова їжа', 2),
+(2, N'Політика в Україні', 3);
+
+insert into Users (id, UserName) values
+(0, 'petro1977'),
+(1, 'lenskiy_the_great');
+
+insert into InterestsToUsers (UserId, InterestId) values
+(0, 0),
+(0, 1),
+(0, 3),
+(1, 0),
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4);
---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+-- Create tables --
+DROP TABLE IF EXISTS InterestsToUsers;
+DROP TABLE IF EXISTS Forums;
+DROP TABLE IF EXISTS Interests;
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Interests
+(
+  id INT PRIMARY KEY,
+  Name VARCHAR(50) NOT NULL UNIQUE,
+);
+
+CREATE TABLE Forums
+(
+  id INT PRIMARY KEY,
+  Name NVARCHAR(50) NOT NULL UNIQUE,
+  InterestId INT NOT NULL FOREIGN KEY REFERENCES Interests(id)
+);
+
+
+CREATE TABLE Users
+(
+  id INT PRIMARY KEY,
+  UserName VARCHAR(50) NOT NULL UNIQUE,
+);
+
+CREATE TABLE InterestsToUsers
+(
+  UserId INT NOT NULL FOREIGN KEY REFERENCES Users(id),
+  InterestId INT NOT NULL FOREIGN KEY REFERENCES Interests(id),
+  PRIMARY KEY (UserId, InterestId)
+);
